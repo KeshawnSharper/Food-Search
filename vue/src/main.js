3840bbca887576f8088bb79081514f8ac2208579
Lambda_School_Loaner_182 <lambda_school_loaner_182@Lambda-School-Loaner-182s-MacBook-Pro.local>
2021-01-31 20:01:15 -0500

    save

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,5 +1,32 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 
-createApp(App).mount('#app')
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
+
+// 2. Define some routes
+// Each route should map to a component. The "component" can
+// either be an actual component constructor created via
+// `Vue.extend()`, or just a component options object.
+// We'll talk about nested routes later.
+const routes = [
+    { path: "/login", component: Login, name: 'login' },
+    { path: "/register", component: Register, name: 'register' }
+];
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes // short for `routes: routes`
+});
+
+new Vue({
+  render: (h) => h(App),
+  router
+}).$mount("#app");
